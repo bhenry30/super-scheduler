1e1b6aa3721d97e1b551d49516d982dce54cb240
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-01-31 12:31:39 -0600

    set up files and added current date to top of calendar

---
+++ assets/js/home.js
@@ -0,0 +1,31 @@
+// Pseudo Code
+// 1. Display the current date at the top of the calendar
+// 	-google: how to get the current date in javascript
+// 		-how to use dates in javascript
+// 2. Render time blocks
+// 	-hard code in html using rows and cols
+// 	-loop over and render the table rows and cols in javascript
+// 3. Color-code time blocks for past, present and future
+// 	-javascript, css
+// 	-if then statements that relate to the date and apply certain css properties
+// 	(select html elements in javascript - and add css to it)
+// 4. Allow user to type in event in timeblocks
+// 	-input fields, with text saved in local storage
+// 	-attribute that indicates time-block on input fields
+// 	-On SAVE: store the date/time as the key and the event as the value
+// 		-create an event listener for the save button that puts this key, 	
+// 		value pair into local storage
+// 5.On refresh, grab the data from local storage and display it in the apppropriate time block.
+
+// Gets current date
+const currentDate = new Date();
+
+// var dateDisplay = $("<h1>").addClass(date-display).text(currentDate)
+
+// Displays current date at top of calendar
+function displayDate() {
+$("#currentDay").text(currentDate)
+};
+
+displayDate();
+
